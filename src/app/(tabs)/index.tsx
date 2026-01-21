184ef3bab206249632492fef346b33a9a561c2c8
laq <1991182634@qq.com>
2026-01-21 17:17:23 +0800

    [Update]Home页面 && eas

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,82 @@
-import { View } from 'react-native';
+import { Dimensions, FlatList, View, ViewToken, StyleSheet } from "react-native"
 import PostListItem from '../components/PostListItem';
+import posts from '../../../assets/data/posts.json';
+import React, { useRef, useState } from 'react';
+import FeedTab from "../components/GenericComponents/FeedTab"
+import { MaterialIcons, Ionicons } from "@expo/vector-icons"
+
+const TABS = {
+  EXPLORE: 'Explore',
+  FOLLOWING: 'Following',
+  FOR_YOU: 'For You',
+}
 
 export default function HomeScreen() { 
+  const { height } = Dimensions.get('window');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeTab, setActiveTab] = useState(TABS.FOR_YOU);
+
+  const onViewableItemsChanged = useRef(({ viewableItems }: { viewableItems: ViewToken[] }) => {
+    if (viewableItems.length > 0) {
+      setCurrentIndex(viewableItems[0].index || 0);
+    }
+  })
+
+  console.log("Current Index:", currentIndex);
+
   return (
     <View>
-      <PostListItem />
+      <View>
+        <View style={styles.topBar}>
+          <MaterialIcons name="live-tv" size={24} color="white" />
+          <View style={styles.navigationBar}>
+            <FeedTab
+              title={TABS.EXPLORE}
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+            <FeedTab
+              title={TABS.FOLLOWING}
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+            <FeedTab
+              title={TABS.FOR_YOU}
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+          </View>
+          <Ionicons name="search" size={24} color="white" />
+        </View>
+      </View>
+
+      <FlatList
+        data={posts}
+        renderItem={({ item, index }) => (
+          <PostListItem postItem={item} isActive={index === currentIndex} />
+        )}
+        showsVerticalScrollIndicator={false}
+        snapToInterval={height - 80}
+        decelerationRate={"fast"}
+        disableIntervalMomentum
+        onViewableItemsChanged={onViewableItemsChanged.current}
+      />
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  navigationBar: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "center",
+    gap: 30
+  },
+  topBar: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 70,
+    zIndex: 1,
+    paddingHorizontal: 15,
+  }
+})
--- src/app/components/PostListItem.tsx
+++ src/app/components/PostListItem.tsx
@@ -1,36 +1,149 @@
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { useVideoPlayer, VideoView } from 'expo-video';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from "react-native"
+import { useVideoPlayer, VideoView } from "expo-video"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import { Post } from "../types/types"
+import { useFocusEffect } from "expo-router"
+import { useCallback } from "react"
 
-const videoSource =
-  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+type VideoItemProps = {
+  postItem: Post
+  isActive: boolean
+}
 
-export default function PostListItem() { 
-  const { height } = Dimensions.get('window');
-  const player = useVideoPlayer(videoSource, player => {
-    player.loop = true;
-    player.play();
-  });
+export default function PostListItem({ postItem, isActive }: VideoItemProps) {
+  const { height } = Dimensions.get("window")
+  const { nrOfComments, nrOfLikes, nrOfShares, description, user, video_url } =
+    postItem
+  const player = useVideoPlayer(video_url, (player) => {
+    player.loop = true
+    player.play()
+  })
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!player) return
+
+      try {
+        if (isActive) {
+          player.play()
+        }
+      } catch (error) {
+        console.log(error)
+      }
+
+      return () => {
+        try {
+          if (player && isActive) {
+            player.pause()
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }, [isActive, player]),
+  )
 
   return (
-    <View style={{ height }}>
-      <VideoView style={{ flex: 1 }} player={player} contentFit='cover' nativeControls={false} />
+    <View style={{ height: height - 80 }}>
+      <VideoView
+        style={{ flex: 1 }}
+        player={player}
+        contentFit="cover"
+        nativeControls={false}
+      />
+
+      <View style={styles.interactionBar}>
+        <TouchableOpacity
+          style={styles.interactionButton}
+          onPress={() => console.log("Like Pressed")}
+        >
+          <Ionicons name="heart" size={33} color="white" />
+          <Text style={styles.interactionText}>{nrOfLikes[0]?.count || 0}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.interactionButton}
+          onPress={() => console.log("Comment Pressed")}
+        >
+          <Ionicons name="chatbubble" size={30} color="white" />
+          <Text style={styles.interactionText}>
+            {nrOfComments[0]?.count || 0}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.interactionButton}
+          onPress={() => console.log("Share Pressed")}
+        >
+          <Ionicons name="arrow-redo" size={33} color="white" />
+          <Text style={styles.interactionText}>
+            {nrOfShares[0]?.count || 0}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.avatar}
+          onPress={() => console.log("Profile Pressed")}
+        >
+          <Text style={styles.avatarText}>
+            {user?.username.charAt(0).toUpperCase()}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.videoInfo}>
+        <Text style={styles.username}>{user?.username}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 50,
+  interactionBar: {
+    position: "absolute",
+    right: 20,
+    bottom: 20,
+    alignItems: "center",
+    gap: 25,
   },
-  video: {
-    width: 350,
-    height: 275,
+  interactionButton: {
+    alignItems: "center",
+    gap: 5,
   },
-  controlsContainer: {
-    padding: 10,
+  interactionText: {
+    color: "white",
+    fontSize: 12,
+    fontWeight: "600",
   },
-});
+  avatar: {
+    width: 35,
+    height: 35,
+    backgroundColor: "white",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  avatarText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  videoInfo: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 100,
+    gap: 5,
+  },
+  username: {
+    color: "white",
+    fontWeight: "600",
+  },
+  description: {
+    color: "white",
+  },
+})
