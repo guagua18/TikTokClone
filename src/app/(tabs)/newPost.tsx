a89bdc020dc861efeba6d4abd589a323948dedcf
laq <1991182634@qq.com>
2026-01-21 17:41:54 +0800

    [Update]new postye访问权限

--- src/app/(tabs)/newPost.tsx
+++ src/app/(tabs)/newPost.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from 'react-native';
+import { View, Text, Linking, Button, StyleSheet } from "react-native"
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera"
+import { useEffect, useState } from "react"
 
 export default function NewPostScreen() { 
+  const [facing, setFacing] = useState<CameraType>("back")
+  const [permission, requestPermission] = useCameraPermissions()
+
+  useEffect(() => {
+    if (permission && !permission.granted && permission.canAskAgain) {
+      requestPermission()
+    }
+  }, [permission])
+
+  if (!permission || !permission.granted && !permission.canAskAgain) { 
+    return (
+      <View style={styles.permissionContainer}>
+        <Text style={styles.permissionText}>We need your permission to access the camera</Text>
+        <Button title="Grant Permission" onPress={() => Linking.openSettings()}></Button>
+      </View>
+    )
+  }
+  
   return (
     <View>
       <Text>New Post Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  permissionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  permissionText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: '700'
+  },
+})
